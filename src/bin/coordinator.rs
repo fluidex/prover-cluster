961fa31dbd0faee253fd47f20537e15c0e667421
HAOYUatHZ <haoyu@protonmail.com>
2021-03-23 13:36:07 +0800

    load config

--- src/bin/coordinator.rs
+++ src/bin/coordinator.rs
@@ -1,7 +1,15 @@
+use prover_cluster::config;
+
 fn main() {
     dotenv::dotenv().ok();
     env_logger::init();
     log::info!("prover coordinator started");
+
+    let mut conf = config_rs::Config::new();
+    let config_file = dotenv::var("COORDINATOR_CONFIG").unwrap();
+    conf.merge(config_rs::File::with_name(&config_file)).unwrap();
+    let settings: config::coordinator::Settings = conf.try_into().unwrap();
+    log::debug!("{:?}", settings);
 
     unimplemented!();
 }
