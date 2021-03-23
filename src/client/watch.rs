c1e3ae2e8f4117d5bbdcef018ac3c3f796186e36
HAOYUatHZ <haoyu@protonmail.com>
2021-03-23 17:18:46 +0800

    minor

--- src/client/watch.rs
+++ src/client/watch.rs
@@ -4,7 +4,7 @@ pub struct Watcher {}
 
 impl Watcher {
     pub fn new() -> Self {
-        return Self {};
+        Self {};
     }
 
     pub async fn run(/*mut*/ self, mut watch_req: mpsc::Receiver<WatchRequest>) {
