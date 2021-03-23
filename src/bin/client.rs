dd49a73cbfec9e7b5f795fdbbbcfdd3e683fdfd3
HAOYUatHZ <haoyu@protonmail.com>
2021-03-23 16:17:05 +0800

    WIP

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -12,4 +12,25 @@ fn main() {
     log::debug!("{:?}", settings);
 
     unimplemented!();
+
+    // let client = EthereumGateway::from_config(&settings);
+
+    // let (eth_req_sender, eth_req_receiver) = mpsc::channel(256);
+
+    // let eth_client = EthHttpClient::new(client, settings.contracts.contract_addr);
+    // let watcher = EthWatch::new(eth_client, settings.eth_watch.confirmations_for_eth_event);
+
+    // main_runtime.spawn(watcher.run(eth_req_receiver));
+    // main_runtime.block_on(async move {
+    //     let mut timer = time::interval(Duration::from_secs(1));
+
+    //     loop {
+    //         timer.tick().await;
+    //         eth_req_sender
+    //             .clone()
+    //             .send(EthWatchRequest::PollETHNode)
+    //             .await
+    //             .expect("ETH watch receiver dropped");
+    //     }
+    // });
 }
