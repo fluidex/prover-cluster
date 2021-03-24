6c29a434683b827c21301e8e1d8117e179158b30
HAOYUatHZ <haoyu@protonmail.com>
2021-03-24 14:43:49 +0800

    init prover

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,2 +1,3 @@
 pub mod config;
+pub mod prover;
 pub mod watch;
---
+++ src/client/prover.rs
@@ -0,0 +1,13 @@
+pub struct Prover {}
+
+impl Default for Prover {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Prover {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
