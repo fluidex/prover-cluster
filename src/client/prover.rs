68cfa001b34f098e361a14556f663dfab62a33ce
HAOYUatHZ <haoyu@protonmail.com>
2021-03-25 22:06:47 +0800

    minor

--- src/client/prover.rs
+++ src/client/prover.rs
@@ -1,16 +1,21 @@
 use crate::client::Settings;
 use crate::pb::*;
+use crate::pb;
 use bellman_ce::{
     pairing::bn256::Bn256,
     plonk::better_cs::{cs::PlonkCsWidth4WithNextStepParams, keys::Proof},
 };
 use std::{thread, time};
 
-pub struct Prover {}
+pub struct Prover {
+    circuit: pb::Circuit,
+}
 
 impl Prover {
-    pub fn from_config(_config: &Settings) -> Self {
-        Self {}
+    pub fn from_config(config: &Settings) -> Self {
+        Self {
+            circuit: config.circuit(),
+        }
     }
 
     pub async fn prove(&self, _task: &Task) -> Result<Proof<Bn256, PlonkCsWidth4WithNextStepParams>, anyhow::Error> {
