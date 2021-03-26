e8fc82e496ed94c55079044abea91102b5468aa6
HAOYUatHZ <haoyu@protonmail.com>
2021-03-26 17:09:59 +0800

    minor

--- src/coordinator/coordinator.rs
+++ src/coordinator/coordinator.rs
@@ -20,18 +20,18 @@ impl Coordinator {
 #[tonic::async_trait]
 impl Cluster for Coordinator {
     async fn poll_task(&self, _request: Request<PollTaskRequest>) -> Result<Response<Task>, Status> {
-        // unimplemented!()
+        // Ok(Response::new(Task {
+        //     id: 1.to_string(),
+        //     circuit: 1,
+        //     witness: vec![],
+        // }))
 
-        Ok(Response::new(Task {
-            id: 1.to_string(),
-            circuit: 1,
-            witness: vec![],
-        }))
+        unimplemented!()
     }
 
     async fn submit_proof(&self, _request: Request<SubmitProofRequest>) -> Result<Response<SubmitProofResponse>, Status> {
-        // unimplemented!()
+        // Ok(Response::new(SubmitProofResponse { valid: true }))
 
-        Ok(Response::new(SubmitProofResponse { valid: true }))
+        unimplemented!()
     }
 }
