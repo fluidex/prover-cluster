4e12b951297290c123f7a85ff4a41947db2a18a1
HAOYUatHZ <haoyu@protonmail.com>
2021-03-26 13:59:14 +0800

    init for plonkit

--- src/client/prover.rs
+++ src/client/prover.rs
@@ -4,15 +4,19 @@ use bellman_ce::{
     pairing::bn256::Bn256,
     plonk::better_cs::{cs::PlonkCsWidth4WithNextStepParams, keys::Proof},
 };
+use plonkit::plonk::SetupForProver;
 use std::{thread, time};
 
 pub struct Prover {
-    circuit: pb::Circuit,
+    circuit_name: pb::Circuit,
+    setup: SetupForProver,
 }
 
 impl Prover {
     pub fn from_config(config: &Settings) -> Self {
-        Self { circuit: config.circuit() }
+        Self {
+            circuit_name: config.circuit(),
+        }
     }
 
     pub async fn prove(&self, _task: &Task) -> Result<Proof<Bn256, PlonkCsWidth4WithNextStepParams>, anyhow::Error> {
