2ec0f9d454988876342762a68375d03da33b3ae5
HAOYUatHZ <haoyu@protonmail.com>
2021-03-28 21:45:14 +0800

    minor

--- src/coordinator/controller.rs
+++ src/coordinator/controller.rs
@@ -16,23 +16,11 @@ impl Controller {
     }
 
     pub fn poll_task(&mut self, request: PollTaskRequest) -> Result<Task, Status> {
-        let circuit = Circuit::from_i32(request.circuit).ok_or_else(|| Status::new(Code::InvalidArgument, "unknown circuit"))?;
+        unimplemented!();
 
-        let tasks: BTreeMap<String, Task> = self
-            .tasks
-            .clone()
-            .into_iter()
-            .filter(|(_id, t)| t.circuit == circuit as i32)
-            .collect();
+        // TODO: pop task
 
-        // match self.controller.fetch_task(circuit) {
-        match tasks.into_iter().next() {
-            None => Err(Status::new(Code::ResourceExhausted, "no task ready to prove")),
-            Some((task_id, task)) => {
-                // self.controller.assign(request.prover_id, task_id);
-                Ok(task)
-            }
-        }
+        // TODO: assign task
     }
 
     pub fn submit_proof(&mut self, _req: SubmitProofRequest) -> Result<SubmitProofResponse, Status> {
@@ -40,7 +28,7 @@ impl Controller {
 
         // TODO: validate proof
 
-        // TODO: store_proof
+        // TODO: store proof
 
         // Ok(SubmitProofResponse { valid: true })
     }
