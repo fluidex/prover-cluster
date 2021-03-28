1333d505ebc0c7cb171caece43a985e5f54b7375
HAOYUatHZ <haoyu@protonmail.com>
2021-03-28 20:40:56 +0800

    some cleaning

--- src/coordinator/controller.rs
+++ src/coordinator/controller.rs
@@ -1,7 +1,7 @@
 use crate::coordinator::Settings;
 use crate::pb::*;
 use std::collections::BTreeMap;
-use tonic::Status;
+use tonic::{Code, Status};
 
 #[derive(Debug, Clone)]
 pub struct Controller {
@@ -9,14 +9,13 @@ pub struct Controller {
 }
 
 impl Controller {
-    // TODO: return error
+    // TODO: async & return error
     pub fn from_config(_config: &Settings) -> Self {
         Self { tasks: BTreeMap::new() }
     }
 
     pub fn poll_task(&mut self, request: PollTaskRequest) -> Result<Task, Status> {
-        let circuit =
-            Circuit::from_i32(request.circuit).ok_or_else(|| tonic::Status::new(tonic::Code::InvalidArgument, "unknown circuit"))?;
+        let circuit = Circuit::from_i32(request.circuit).ok_or_else(|| Status::new(Code::InvalidArgument, "unknown circuit"))?;
 
         let tasks: BTreeMap<String, Task> = self
             .tasks
@@ -27,7 +26,7 @@ impl Controller {
 
         // match self.controller.fetch_task(circuit) {
         match tasks.into_iter().next() {
-            None => Err(tonic::Status::new(tonic::Code::ResourceExhausted, "no task ready to prove")),
+            None => Err(Status::new(Code::ResourceExhausted, "no task ready to prove")),
             Some((task_id, task)) => {
                 // self.controller.assign(request.prover_id, task_id);
                 Ok(task)
@@ -40,7 +39,7 @@ impl Controller {
 
         // TODO: validate proof
 
-        // self.controller.store_proof(request);
+        // TODO: store_proof
 
         // Ok(SubmitProofResponse { valid: true })
     }
