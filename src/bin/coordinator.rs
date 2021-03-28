a7ec6082788f99a896030c5358915d3356a91341
HAOYUatHZ <haoyu@protonmail.com>
2021-03-28 18:28:52 +0800

    WIP

--- src/bin/coordinator.rs
+++ src/bin/coordinator.rs
@@ -2,8 +2,7 @@ use prover_cluster::coordinator::{config, Coordinator};
 use prover_cluster::pb::cluster_server::ClusterServer;
 use tonic::transport::Server;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     dotenv::dotenv().ok();
     env_logger::init();
     log::info!("prover coordinator started");
@@ -14,9 +13,53 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let settings: config::Settings = conf.try_into().unwrap();
     log::debug!("{:?}", settings);
 
-    let addr = format!("[::1]:{:?}", settings.port).parse().unwrap();
-    let coordinator = Coordinator::from_config(&settings);
-    Server::builder().add_service(ClusterServer::new(coordinator)).serve(addr).await?;
+    // let addr = format!("[::1]:{:?}", settings.port).parse().unwrap();
+    // let coordinator = Coordinator::from_config(&settings);
+    // Server::builder().add_service(ClusterServer::new(coordinator)).serve(addr).await?;
 
+    // Ok(())
+
+    let main_runtime: tokio::runtime::Runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .expect("build runtime");
+
+    main_runtime.block_on(async {
+        let server = prepare().await.expect("Init state error");
+        grpc_run(server).await
+    })
+    .unwrap();
+}
+
+async fn prepare() -> anyhow::Result<Coordinator> {
+    let mut grpc_stub = create_controller(settings);
+    let grpc = Coordinator::new(grpc_stub);
+    Ok(grpc)
+}
+
+async fn grpc_run(mut grpc: Coordinator) -> Result<(), Box<dyn std::error::Error>> {
+    // let addr = "0.0.0.0:50051".parse().unwrap();
+    // log::info!("Starting gprc service");
+
+    let (tx, rx) = tokio::sync::oneshot::channel::<()>();
+    let on_leave = grpc.on_leave();
+
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.ok();
+        log::info!("Ctrl-c received, shutting down");
+        tx.send(()).ok();
+    });
+
+    tonic::transport::Server::builder()
+        // TODO:???
+        .add_service(Coordinator::new(grpc))
+        .serve_with_shutdown(grpc.addr, async {
+            rx.await.ok();
+        })
+        .await?;
+
+    log::info!("Shutted down, wait for final clear");
+    on_leave.leave().await;
+    log::info!("Shutted down");
     Ok(())
 }
