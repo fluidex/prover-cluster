87012df5563ea940e9c94695f2d8b440cdefb54f
HAOYUatHZ <haoyu@protonmail.com>
2021-03-31 16:04:55 +0800

    minor

--- src/coordinator/controller.rs
+++ src/coordinator/controller.rs
@@ -35,6 +35,7 @@ impl Controller {
         match task {
             None => Err(Status::new(Code::ResourceExhausted, "no task ready to prove")),
             Some(t) => {
+                // self.tasks.remove(&task_id);
                 self.assign_task(t.task_id, request.prover_id);
                 Ok(Task {
                     circuit: request.circuit,
