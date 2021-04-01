f2955fc35c00a87e6db991f6bbf03b7ae382348e
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2021-04-01 16:43:06 +0800

    fix db schema (#35)

--- migrations/20210330142700_task.sql
+++ migrations/20210330142700_task.sql
@@ -1,12 +1,14 @@
+CREATE TYPE task_status AS ENUM('not_assigned', 'assigned', 'proved');
+
 CREATE TABLE task (
     task_id VARCHAR(30) NOT NULL,
     circuit VARCHAR(30) NOT NULL,
     witness TEXT NOT NULL,
     proof TEXT,
-    -- status TEXT NOT NULL,
+    status task_status NOT NULL DEFAULT 'not_assigned',
     prover_id VARCHAR(30),
     created_time TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_time TIMESTAMP(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+    updated_time TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (task_id)
 );
 
@@ -14,7 +16,14 @@ CREATE TABLE task (
 -- CREATE INDEX task_idx_status ON task (status);
 -- CREATE UNIQUE INDEX task_prover_constraint ON task (task_id, prover_id);
 
-CREATE TYPE task_status AS ENUM('not_assigned', 'assigned', 'proved');
+CREATE OR REPLACE FUNCTION update_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+   NEW.updated_time = CURRENT_TIMESTAMP; 
+   RETURN NEW;
+END;
+$$ language 'plpgsql';
 
--- Set task as not_assigned by default
-ALTER TABLE task ADD status order_status NOT NULL DEFAULT 'not_assigned';
+CREATE TRIGGER update_timestamp BEFORE UPDATE
+ON task FOR EACH ROW EXECUTE PROCEDURE
+update_timestamp();
