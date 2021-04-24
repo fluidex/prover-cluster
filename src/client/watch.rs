5c9b9234ecdfffa6201392a0950587f63fb292b9
Zequn Ma <mazequn@hotmail.com>
2021-04-24 10:34:51 +1000

    fix fmt

--- src/client/watch.rs
+++ src/client/watch.rs
@@ -39,18 +39,18 @@ impl Watcher {
 
                     match self.prover.prove(&task).await {
                         Ok(proof) => {
-							let grpc_client_clone = self.grpc_client.clone();
-							thread::spawn(move || async move {
-                            	match grpc_client_clone.submit(&task.id, proof).await {
-                                	Ok(resp) => {
-                                    	log::info!("submission for task({:?}) successful", &task.id);
-                                    	log::info!("task({:?}) submission result valid: {:?}", &task.id, resp.valid);
-                                	}
-                                	Err(e) => {
-                                    	log::error!("submit result for task({:?}) error {:?}", &task.id, e);
-                                	}
-                            	};
-							});
+                            let grpc_client_clone = self.grpc_client.clone();
+                            thread::spawn(move || async move {
+                                match grpc_client_clone.submit(&task.id, proof).await {
+                                    Ok(resp) => {
+                                        log::info!("submission for task({:?}) successful", &task.id);
+                                        log::info!("task({:?}) submission result valid: {:?}", &task.id, resp.valid);
+                                    }
+                                    Err(e) => {
+                                        log::error!("submit result for task({:?}) error {:?}", &task.id, e);
+                                    }
+                                };
+                            });
                         }
                         Err(e) => log::error!("{:?}", e),
                     }
