3b1a2bf328e35156da74043abd8c70e8d2c89a1b
HAOYUatHZ <haoyu@protonmail.com>
2021-05-08 20:49:24 +0800

    WitnessFactory return error

--- src/coordinator/witness_factory.rs
+++ src/coordinator/witness_factory.rs
@@ -1,5 +1,6 @@
 use crate::coordinator::db::{models, DbType, PoolOptions};
 use crate::coordinator::Settings;
+use anyhow::{anyhow, bail};
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
@@ -9,7 +10,7 @@ use std::process::Command;
 use std::time::Duration;
 use tempfile::tempdir;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct WitnessFactory {
     db_pool: sqlx::Pool<DbType>,
     witgen_interval: Duration,
@@ -35,68 +36,83 @@ impl WitnessFactory {
         })
     }
 
-    pub async fn run(mut self) {
+    pub async fn run(self) {
         let mut timer = tokio::time::interval(self.witgen_interval);
 
         // TODO: use worker_pool for multiple workers
         loop {
             timer.tick().await;
             log::debug!("ticktock!");
-
-            let task = self.claim_one_task().await.expect("claim_one_task");
-            if task.is_none() {
-                continue;
-            }
-            let task = task.unwrap();
-            log::info!("get 1 task to generate witness");
-
-            // create temp dir
-            let dir = tempdir().expect("create tempdir in std::env::temp_dir()");
-            log::info!("process in tempdir path: {:?}", dir.path());
-
-            let inputjson = format!("{}", task.input);
-            log::debug!("inputjson content: {:?}", inputjson);
-
-            // save inputjson to file
-            let inputjson_filepath = dir.path().join("input.json");
-            log::debug!("inputjson_filepath: {:?}", inputjson_filepath);
-            let mut inputjson_file = File::create(inputjson_filepath.clone()).expect("create input.json");
-            inputjson_file.write_all(inputjson.as_bytes()).expect("save input.json");
-
-            let witness_filepath = dir.path().join("witness.wtns");
-            log::debug!("witness_filepath: {:?}", witness_filepath);
-
-            // decide circuit
-            let circuit_name = format!("{:?}", task.circuit).to_lowercase();
-            log::debug!("circuit_name: {:?}", circuit_name);
-            if self.circuits.get(&circuit_name).is_none() {
-                log::error!("unknown circuit: {:?}", circuit_name);
-                continue;
-            }
-            let circuit = self.circuits.get(&circuit_name).unwrap();
-
-            // execute circuit binary & wait for the execution
-            Command::new(circuit)
-                .arg(inputjson_filepath.as_os_str())
-                .arg(witness_filepath.as_os_str())
-                .status()
-                .expect("failed to execute circuit binary");
-
-            // read from witness
-            let mut witness_file = File::open(witness_filepath).expect("open witness.wtns");
-            let mut witness = Vec::new();
-            // read the whole file
-            witness_file.read_to_end(&mut witness).expect("read witness.wtns");
-
-            // save to DB
-            self.save_wtns_to_db(task.clone().task_id, witness)
-                .await
-                .expect("save witness to db");
-
-            // TODO: handle offline workers (clean up Witgening tasks)
-
-            log::info!("task (id: {:?}) witness save successfully!", task.task_id);
+            match self.clone().run_inner().await {
+                Err(e) => {
+                    log::error!("{}", e);
+                }
+                _ => {}
+            };
         }
+    }
+
+    async fn run_inner(mut self) -> Result<(), anyhow::Error> {
+        let task = self.claim_one_task().await.map_err(|_| anyhow!("claim_one_task"))?;
+        if task.is_none() {
+            return Ok(());
+        }
+        let task = task.unwrap();
+        log::info!("get 1 task to generate witness");
+
+        // create temp dir
+        let dir = tempdir().map_err(|_| anyhow!("create tempdir in std::env::temp_dir()"))?;
+        log::info!("process in tempdir path: {:?}", dir.path());
+
+        let inputjson = format!("{}", task.input);
+        log::debug!("inputjson content: {:?}", inputjson);
+
+        // save inputjson to file
+        let inputjson_filepath = dir.path().join("input.json");
+        log::debug!("inputjson_filepath: {:?}", inputjson_filepath);
+        let mut inputjson_file = File::create(inputjson_filepath.clone()).map_err(|_| anyhow!("create input.json"))?;
+        inputjson_file
+            .write_all(inputjson.as_bytes())
+            .map_err(|_| anyhow!("save input.json"))?;
+
+        let witness_filepath = dir.path().join("witness.wtns");
+        log::debug!("witness_filepath: {:?}", witness_filepath);
+
+        // decide circuit
+        let circuit_name = format!("{:?}", task.circuit).to_lowercase();
+        log::debug!("circuit_name: {:?}", circuit_name);
+        if self.circuits.get(&circuit_name).is_none() {
+            bail!("unknown circuit: {:?}", circuit_name);
+        }
+        let circuit = if let Some(circuit) = self.circuits.get(&circuit_name) {
+            circuit
+        } else {
+            bail!("unknown circuit: {:?}", circuit_name);
+        };
+
+        // execute circuit binary & wait for the execution
+        Command::new(circuit)
+            .arg(inputjson_filepath.as_os_str())
+            .arg(witness_filepath.as_os_str())
+            .status()
+            .map_err(|_| anyhow!("failed to execute circuit binary"))?;
+
+        // read from witness
+        let mut witness_file = File::open(witness_filepath).map_err(|_| anyhow!("open witness.wtns"))?;
+        let mut witness = Vec::new();
+        // read the whole file
+        witness_file.read_to_end(&mut witness).map_err(|_| anyhow!("read witness.wtns"))?;
+
+        // save to DB
+        self.save_wtns_to_db(task.clone().task_id, witness)
+            .await
+            .map_err(|_| anyhow!("save witness to db"))?;
+
+        // TODO: handle offline workers (clean up Witgening tasks)
+
+        log::info!("task (id: {:?}) witness save successfully!", task.task_id);
+
+        Ok(())
     }
 
     async fn claim_one_task(&mut self) -> Result<Option<models::Task>, anyhow::Error> {
