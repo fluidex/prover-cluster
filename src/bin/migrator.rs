848506da7fcd0648aeeedfe1b9f1da8b6118e6ca
HAOYUatHZ <haoyu@protonmail.com>
2021-06-24 19:10:48 +0800

    cargo fmt

--- src/bin/migrator.rs
+++ src/bin/migrator.rs
@@ -3,11 +3,10 @@ use sqlx::Connection;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let db_url: String =
-        std::env::var("DB_URL")
-            .unwrap_or_else(|_| "postgres://coordinator:coordinator_AA9944@127.0.0.1/prover_cluster".to_string())
-                .parse::<String>()
-                .expect("parse DB_URL");
+    let db_url: String = std::env::var("DB_URL")
+        .unwrap_or_else(|_| "postgres://coordinator:coordinator_AA9944@127.0.0.1/prover_cluster".to_string())
+        .parse::<String>()
+        .expect("parse DB_URL");
 
     let mut db_conn = ConnectionType::connect(&db_url).await?;
     MIGRATOR.run(&mut db_conn).await?;
