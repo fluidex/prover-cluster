dffd6220566660e92f143e0ed5760e781ff9c466
HAOYUatHZ <haoyu@protonmail.com>
2021-06-28 10:50:07 +0800

    minor

--- src/coordinator/db/mod.rs
+++ src/coordinator/db/mod.rs
@@ -3,8 +3,6 @@ pub type ConnectionType = sqlx::postgres::PgConnection;
 pub type PoolOptions = sqlx::postgres::PgPoolOptions;
 pub type DBErrType = sqlx::Error;
 
-// TODO: migrate
-
 pub mod models;
 
 pub static MIGRATOR: sqlx::migrate::Migrator = sqlx::migrate!(); // defaults to "./migrations"
