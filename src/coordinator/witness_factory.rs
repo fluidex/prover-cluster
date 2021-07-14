5866c039c2a1f134417cedb2c5d7624bea26d474
HAOYUatHZ <haoyu@protonmail.com>
2021-07-14 14:01:29 +0800

    witness_factory: fix claim_tasks query

--- src/coordinator/witness_factory.rs
+++ src/coordinator/witness_factory.rs
@@ -137,7 +137,7 @@ impl WitnessFactory {
         let mut tx = self.db_pool.begin().await?;
 
         let query = format!(
-            "select task_id, circuit, input, output, witness, public_input, proof, status, prover_id, created_time, updated_time
+            "select task_id, circuit, block_id, input, output, witness, public_input, proof, status, prover_id, created_time, updated_time
             from {}
             where status = $1
             limit {}",
