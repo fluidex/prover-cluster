1d2662a5e547e231a1ac1e5cb0d9427e2d5b7198
Steven <asongala@163.com>
2021-07-29 17:07:36 +0800

    Uses MIGRATOR, `tablenames::TASK` and task model types of `common-rs`. (#92)

--- src/pb.rs
+++ src/pb.rs
@@ -1,1 +1,11 @@
 tonic::include_proto!("cluster");
+
+use fluidex_common::db::models::task::CircuitType;
+
+impl From<Circuit> for CircuitType {
+    fn from(pb_circuit: Circuit) -> Self {
+        match pb_circuit {
+            Circuit::Block => Self::BLOCK,
+        }
+    }
+}
