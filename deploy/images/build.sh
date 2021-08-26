c5303eb4d20f2670f37e3488c72fba16928dfcae
HAOYUatHZ <haoyu@protonmail.com>
2021-08-26 14:58:21 +0800

    remove deployment stuff

--- deploy/images/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker build -f setup.docker . -t setup
-docker build -f plonkit.docker . -t plonkit
-docker build -f prover.docker . -t prover
-docker build -f cluster_client.docker . -t cluster_client
-docker build -f cluster_client_test.docker . -t cluster_client_test
-docker build -f cluster_coordinator.docker . -t cluster_coordinator
