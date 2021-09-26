76f303d9bb758c582b7b99bf2915ca879cf2118b
HAOYUatHZ <haoyu@protonmail.com>
2021-09-26 14:02:25 +0800

    fix

--- test/mock.sql
+++ test/mock.sql
@@ -1,4 +1,2 @@
-insert into task (task_id, circuit, input) values 
-    ('6', 'block', '{ "foo": 13, "bar": 4 }::jsonb'),
-    ('7', 'block', '{ "foo": 14, "bar": 4 }::jsonb'),
-    ('8', 'block', '{ "foo": 15, "bar": 4 }::jsonb');
+insert into task (task_id, circuit, block_id, input, status) values (
+'task_1', 'block', '1', '{ "foo": 3, "bar": 4 }'::jsonb, 'inited');
