0e2d32bde826708d31e9c73713c7a40347a49035
HAOYUatHZ <haoyu@protonmail.com>
2021-09-26 12:30:07 +0800

    add kill_tasks

--- test/stop.sh
+++ test/stop.sh
@@ -4,6 +4,12 @@ set -uex
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 REPO_DIR=$DIR/".."
 
+function kill_tasks() {
+  # kill last time running tasks:
+  ps aux | grep 'prover-cluster' | grep -v grep | awk '{print $2 " " $11}'
+  kill -9 $(ps aux | grep 'prover-cluster' | grep -v grep | awk '{print $2}') || true
+}
+
 function stop_docker_compose() {
   dir=$1
   name=$2
@@ -13,5 +19,6 @@ function stop_docker_compose() {
 }
 
 if [[ -z ${AS_RESOURCE+x}  ]]; then
+  kill_tasks
   stop_docker_compose $REPO_DIR prover_cluster
 fi
